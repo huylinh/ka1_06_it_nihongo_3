b308faf2ca684d28c563f58d7d751d685947512d
HuyLinh <nguyenhuylinhvt98@gmail.com>
2023-02-11 14:51:08 +0700

    add user profile

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2023_02_11_070257) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.string "phone"
+    t.string "address"
+    t.integer "expensed_money"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
