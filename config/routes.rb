eab8a2dd5afe1616f859315f65c9891286dbd798
ThienHD19 <thienhd4.hmc@gmail.com>
2023-02-13 05:48:25 +0000

    add product

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
 
   get 'bill', to: 'bill#show' # /bill
   post 'bill', to: 'bill#create'
-  get 'cart', to: 'cart#show', as: 'cart_path'
-  post 'cart/add', to: 'cart#add', as: 'add_to_cart'
   
+  get 'cart', to: 'cart#show', as: 'cart'
+  post 'cart', to: 'cart#add', as: 'add_to_cart'
+  patch 'cart/:product_id', to: 'cart#edit_quantity', as: 'edit_quantity_cart'
+  delete 'cart/:product_id', to: 'cart#delete', as: 'delete_product_cart'
 
 end
 
